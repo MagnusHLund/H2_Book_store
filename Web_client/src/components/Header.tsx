49a10c2b9c3cc10876d6e11356328c116ab6e5e6
Jorjo6712 <kiro.nicolov@gmail.com>
2024-04-23 13:22:54 +0200

    new

---
+++ Web_client/src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+function Header() {
+
+    return <>
+        <div className="header">
+            <h1 className="logo"> Test </h1>
+            <div className="searchBox">
+                <input type="text" className="search"></input>
+            </div>
+            <button> Log In</button>
+        </div>
+    </>
+}
+
+export default Header
