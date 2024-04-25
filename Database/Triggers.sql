026681508840ac94166b0633d9dffea3969e8922
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-04-25 20:28:13 +0200

    Database changes

--- Database/Triggers.sql
+++ Database/Triggers.sql
@@ -4,28 +4,3 @@ CREATE DEFINER=`BookClub`@`%` TRIGGER `decrease_stock` AFTER INSERT ON `OrderIte
    SET stock = stock - NEW.quantity
    WHERE book_id = NEW.book_id;
 END
-
--- This Trigger updates the total price inside of orders, whenever an insert is performed in the OrderItems table.
-CREATE DEFINER=`BookClub`@`%` TRIGGER `update_total_price` AFTER INSERT ON `OrderItems` FOR EACH ROW BEGIN
-   DECLARE discount TINYINT;
-   DECLARE total FLOAT;
-
-   -- Get the discount of the coupon used in the order, if any
-   SELECT discount_percentage INTO discount
-   FROM Coupons
-   JOIN Orders ON Coupons.coupon_id = Orders.coupon_id
-   WHERE Orders.order_id = NEW.order_id;
-
-   -- Calculate the total price of the new order item
-   SET total = NEW.quantity * NEW.price;
-
-   -- Apply the discount, if any
-   IF discount IS NOT NULL THEN
-      SET total = total * (1 - discount / 100);
-   END IF;
-
-   -- Update the total price of the order
-   UPDATE Orders
-   SET total_price = total_price + total
-   WHERE order_id = NEW.order_id;
-END;
