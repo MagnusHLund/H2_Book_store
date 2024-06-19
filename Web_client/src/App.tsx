fae78e9a5f8d3233b41b3a2482aee05b115d0b3a
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-06-19 11:39:37 +0200

    Added thank you page routing

--- Web_client/src/App.tsx
+++ Web_client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.scss'
 import { Route, useLocation, Routes } from 'react-router-dom'
 import Home from './components/sections/HomeSection.tsx'
 import Header from './components/content/Header.tsx'
+import ThankYouSection from './components/sections/ThankYouSection.tsx'
 
 function App() {
   const location = useLocation()
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
+        <Route path="/thankyou" element={<ThankYouSection />} />
       </Routes>
     </>
   )
---
+++ Web_client/src/components/sections/ThankYouSection.tsx
@@ -0,0 +1,15 @@
+import BoughtProducts from './ThankYouSection/BoughtProducts.tsx'
+import HomeButton from './ThankYouSection/HomeButton.tsx'
+import ThankYouText from './ThankYouSection/ThankYouText.tsx'
+
+const ThankYouSection = () => {
+  return (
+    <>
+      <ThankYouText />
+      <BoughtProducts />
+      <HomeButton />
+    </>
+  )
+}
+
+export default ThankYouSection
