b241fb5874df9ced649d2c7acb7f4ca6aadbfd22
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-06-20 10:40:35 +0200

    Added thank you section to route

--- Web_client/src/App.tsx
+++ Web_client/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.scss'
+import { Route, useLocation, Routes } from 'react-router-dom'
+import HomeSection from './components/sections/HomeSection.tsx'
+import Header from './components/content/Header.tsx'
 import ThankYouSection from './components/sections/ThankYouSection/ThankYouSection'
 
 function App() {
+  const location = useLocation()
+
+  const navlinks = [
+    { name: 'Home', path: '/' },
+    { name: 'Login', path: '/login' },
+  ]
 
   return (
     <>
-        <ThankYouSection/>
+      <Header />
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<HomeSection />} />
+        <Route path="/thankYou" element={<ThankYouSection />} />
+      </Routes>
     </>
   )
 }
