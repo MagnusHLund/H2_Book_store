652c6b76b033ad103d1a218095776ba987e4cc7d
MarcusL00 <118048523+MarcusL00@users.noreply.github.com>
2024-06-21 08:23:34 +0200

    fixed
    bim

--- Web_client/src/components/sections/sub sections/Product section/ProductAdditionalInfoSection.tsx
+++ Web_client/src/components/sections/sub sections/Product section/ProductAdditionalInfoSection.tsx
@@ -3,12 +3,12 @@ import './ProductAdditionalInfoSection.scss'
 function ProductAdditionalInfoSection() {
   return (
     <div className="ProductInfo">
-      <div className="ProductInfo-start">
+      <div className="ProductInfo-point">
         <h4>ISBN:</h4>
         <h4>Release date:</h4>
         <h4>Language:</h4>
       </div>
-      <div className="ProductInfo-end">
+      <div className="ProductInfo-text">
         <p>54373293824</p>
         <p>12/05/2008</p>
         <p>Danish</p>
